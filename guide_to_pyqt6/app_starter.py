cb975e5009961e68fc467be161d5fee04e8b68ed
EldenDeDux <wonge265@hsd.k12.or.us>
2026-04-16 08:57:23 -0700

    make calculator

--- guide_to_pyqt6/app_starter.py
+++ guide_to_pyqt6/app_starter.py
@@ -11,6 +11,8 @@ from PyQt6.QtWidgets import (
     QMainWindow,
     QVBoxLayout,
     QWidget,
+    QPushButton,
+    QLineEdit,
 )
 
 
@@ -23,11 +25,31 @@ class MainWindow(QMainWindow):
         self.resize(320, 240)
 
         layout = QVBoxLayout()
-        title_label = QLabel("Title Label (Make this bigger, please!)")
+        title_label = QLabel("Basic Calculator App")
+
+
+        # TODO: add a text input for user's name
+        self.number_1 = QLineEdit(placeholderText = "0")
+        self.number_2 = QLineEdit(placeholderText = "0")
+
+        # TODO: add a push button to greet user
+        addition_button = QPushButton("+")
+        addition_button.clicked.connect(self.get_input_addition)
+        subtraction_button = QPushButton("-")
+        subtraction_button.clicked.connect(self.get_input_subtraction)
+
+        # TODO: add a label to greet user
+        self.instructions = "Enter two numbers, then click one of the buttons."
+        self.output_label = QLabel(self.instructions)
+        self.output_label.setWordWrap(True)
 
         # add widgets & layouts to main layout
         layout.addWidget(title_label)
-
+        layout.addWidget(self.number_1)
+        layout.addWidget(self.number_2)
+        layout.addWidget(addition_button)
+        layout.addWidget(subtraction_button)
+        layout.addWidget(self.output_label)
         # [OPTIONAL] Add a stretch to move everything up
         layout.addStretch()
 
@@ -37,6 +59,31 @@ class MainWindow(QMainWindow):
         # Set the central widget of the Window.
         self.setCentralWidget(widget)
 
+
+
+    def get_input_addition(self):
+        output = ""
+        try:
+            number_1 = float(self.number_1.text())
+            number_2 = float(self.number_2.text())
+            answer = number_1 + number_2
+            output = f"Your answer is {answer}."
+            self.output_label.setText(output)
+        except ValueError:
+            output = "Warning: Please enter valid numbers in both slots."
+            self.output_label.setText(output)
+
+    def get_input_subtraction(self):
+        output = ""
+        try:
+            number_1 = float(self.number_1.text())
+            number_2 = float(self.number_2.text())
+            answer = number_1 - number_2
+            output = f"Your answer is {answer}."
+            self.output_label.setText(output)
+        except ValueError:
+            output = "Warning: Please enter valid numbers in both slots."
+            self.output_label.setText(output)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
